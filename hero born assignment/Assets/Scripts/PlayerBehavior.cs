549ba37ed08fa3cb03fcba7de746d54c16bce77e
tasnuvac <tasnuvac4@gmail.com>
2024-04-21 23:22:24 -0400

    edited player behavior code
    made it so that player can move forward and backward in the direction they are looking at

--- hero born assignment/Assets/Scripts/PlayerBehavior.cs
+++ hero born assignment/Assets/Scripts/PlayerBehavior.cs
@@ -11,11 +11,13 @@ public class PlayerBehavior : MonoBehaviour
     // 2
     private float _vInput;
     private float _hInput;
+
+    private Rigidbody _rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -26,7 +28,8 @@ public class PlayerBehavior : MonoBehaviour
 
          // 4
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
-
+        
+        /*
          // 5
         this.transform.Translate(Vector3.forward * _vInput *
         Time.deltaTime);
@@ -34,5 +37,23 @@ public class PlayerBehavior : MonoBehaviour
         // 6
         this.transform.Rotate(Vector3.up * _hInput *
         Time.deltaTime);
+        */
+    }
+
+    void FixedUpdate()
+    {
+        // 2
+        Vector3 rotation = Vector3.up * _hInput;
+
+        // 3
+        Quaternion angleRot = Quaternion.Euler(rotation *
+        Time.fixedDeltaTime);
+
+        // 4
+        _rb.MovePosition(this.transform.position +
+        this.transform.forward * _vInput * Time.fixedDeltaTime);
+        
+        // 5
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
